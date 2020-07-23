04b34d2673a16d30697891ca772c84c73d4c2dba
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-23 18:10:50 -0400

    create a hook to fetch jobs via GitHub Jobs api, then return the state change and list all titles of each job as a simple text element.

---
+++ Job-Search/src/useFetchJobs.js
@@ -0,0 +1,44 @@
+import {useReducer, useEffect} from 'react';
+import axios from 'axios';
+
+const GITJ_ENDPOINT = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+
+const  ACTIONS = {
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+
+}
+
+function reducer(state, action) {
+    switch( action.type ) {
+        case ACTIONS.MAKE_REQUEST:
+            return { loading: true, jobs: []}
+        case ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs: action.payload.jobs}
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs: []}
+    }
+}
+
+export default (qParams, pageNum ) => {
+    const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true})
+
+    useEffect( () => {
+
+        dispatch( {type: ACTIONS.MAKE_REQUEST} )
+        axios.get(GITJ_ENDPOINT, {
+            params: { ...qParams, page: pageNum, markdown: 1 }
+        })
+        .then( res => {
+            dispatch( { type: ACTIONS.GET_DATA, payload: { jobs: res.data } } )
+        })
+        .catch ( err => {
+            dispatch( { type: ACTIONS.ERROR, payload: { error: err } } )
+        })
+    },
+    [qParams, pageNum]
+    )
+
+    return state
+}
