b0f3a52e616ff49a587860f1f4411519d972aa04
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-24 17:59:31 -0400

    messages are in their own div, add styling constants

--- Job-Search/src/App.js
+++ Job-Search/src/App.js
@@ -6,19 +6,34 @@ import Job from './Job';
 function App() {
 
   const [ params, setParams ] = useState({})
-  const [page, setPage] = useState(1)
+  const [ page, setPage ] = useState(1)
 
-  const {jobs, loading, error } = useFetchJobs( params, page)
+  const { jobs, loading, error } = useFetchJobs( params, page )
 
   return (
-    <Container>
-      {loading && <h1>Loading...</h1>}
-      {error && <h1>An Error Occured, Refresh Page...</h1>}
+    <Container
+      className='my-4'
+    >
+    <div
+      style={{ ...styles.container }}
+    >
+      {loading && <h1 style={{...styles.messageText}}>Loading...</h1>}
+      {error && <h1 style={{...styles.messageText}}>An Error Occured, Refresh Page...</h1>}
+    </div>
       {jobs.map( job => {
         return <Job key={job.id} job={job} />
       })}
     </Container>
   );
+}
+
+const styles = {
+  container: {
+    textAlign: 'center'
+  },
+  messageText: {
+    color: 'white'
+  }
 }
 
 export default App;
