39ea774e56431971fb37a54663ea2a988d8791ba
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-24 18:00:27 -0400

    create collapseable elements detail divs for job application and description, update style

--- Job-Search/src/Job.js
+++ Job-Search/src/Job.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Card, Badge } from 'react-bootstrap'
+import React, {useState} from 'react'
+import { Card, Badge, Collapse, Button } from 'react-bootstrap'
 
 export default function Job({ job }) {
 
+    const [openDetail, setOpenDetail] = useState(false)
+    const [openApply, setOpenApply] = useState(false)
     return (
         <Card style={{...styles.card}}>
             <Card.Body style={{...styles.cardBody}}>
@@ -23,25 +25,85 @@ export default function Job({ job }) {
                             variant='secondary'
                         >{job.type}</Badge>
                         <Badge variant='secondary'>{job.location}</Badge>
-                        <div dangerouslySetInnerHTML={{__html: job.how_to_apply}} />
                     </div>
+                    <img 
+                        style={{...styles.companyLogo}}
+                        className='d-none d-md-block' 
+                        alt={job.company} 
+                        src={job.company_logo} 
+                    />
                 </div>
-            </Card.Body>
+                
+                {/* button to hide/show how to apply details  */}
+                <Card.Text>
+                    <Button
+                        onClick={() => setOpenApply(openVal => !openVal)} 
+                        variant='primary'
+                        style={{...styles.dtlBtn, marginTop: 15}}
+                    >
+                        {
+                            openApply 
+                             ? 'Hide Appliction Details'
+                             : 'How To Apply' 
+                        }
+                    </Button>
+                </Card.Text>
 
+                {/* details about how to apply */}
+                <Collapse in={openApply}>
+                    <div dangerouslySetInnerHTML={{__html: job.how_to_apply}} />
+                </Collapse>
+
+                {/* button to hide/show how to job description  */}
+                <Card.Text>
+                    <Button
+                        onClick={() => setOpenDetail(openVal => !openVal)} 
+                        variant='primary'
+                        style={{...styles.dtlBtn}}
+                    >
+                        {
+                            openApply 
+                            ? 'Hide Job Description'
+                            : 'View Job Description' 
+                        }
+                    </Button>
+                </Card.Text>
+
+                {/* details about how to apply */}
+                <Collapse in={openDetail}>
+                    <div dangerouslySetInnerHTML={{__html: job.description}} />
+                </Collapse>
+
+            </Card.Body>
         </Card>
     )
 }
 
 const styles = {
     card: {
-        backgroundColor: 'lightblue'
+        backgroundColor: 'lightblue',
+        marginBottom: 10,
     },
     cardBody: {
+
         backgroundColor: 'antiquewhite',
         margin: 10
     },
+    dtlBtn: {
+        color: 'white',
+        backgroundColor: '#111',
+        border: 'none'
+    },
     mainInfo: {
 
+    },
+    descDiv: {
+        marginTop: 5,
+    },
+    companyLogo: {
+        borderRadius: 10,
+        maxWidth: 200,
+        maxHeight: 50
     },
     lightText: {
         fontWeight: 'lighter',
