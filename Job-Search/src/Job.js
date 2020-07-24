9eb9b75ccf21f8a32c23f392cfe301a16153a092
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-23 23:29:38 -0400

    get info from github api and create a Card container for each job on the given page

--- Job-Search/src/Job.js
+++ Job-Search/src/Job.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 export default function Job({ job }) {
     return (
-        <div>
-            {job.title}
-        </div>
+        <Card style={{...styles.card}}>
+            <Card.Body style={{...styles.cardBody}}>
+                {job.title} - <span style={{...styles.lightText}}>{job.company}</span>
+            </Card.Body>
+
+        </Card>
     )
+}
+
+const styles = {
+    card: {
+        backgroundColor: 'lightblue'
+    },
+    cardBody: {
+        backgroundColor: 'antiquewhite',
+        margin: 10
+    },
+    lightText: {
+        fontWeight: 'lighter',
+        color: 'grey'
+    }
 }
