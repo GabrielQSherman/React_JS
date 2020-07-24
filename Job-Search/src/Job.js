818a9b8ddc17ad5bf6d531c91d66875ff9ae0979
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-24 15:58:42 -0400

    update Job componenet with more info including how to apply and date created

--- Job-Search/src/Job.js
+++ Job-Search/src/Job.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Badge } from 'react-bootstrap'
 
 export default function Job({ job }) {
+
     return (
         <Card style={{...styles.card}}>
             <Card.Body style={{...styles.cardBody}}>
-                {job.title} - <span style={{...styles.lightText}}>{job.company}</span>
+                <div className='justify-content-between d-flex'>
+                    <div 
+                        style={{...styles.mainInfo}}
+                    >
+                        <Card.Title>
+                            {job.title} - <span style={{...styles.lightText}}>{job.company}</span>
+                        </Card.Title>
+                        <Card.Subtitle
+                            style={{...styles.lightText, marginBottom: 3}}
+                        >
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge 
+                            style={{marginRight: 3}}
+                            variant='secondary'
+                        >{job.type}</Badge>
+                        <Badge variant='secondary'>{job.location}</Badge>
+                        <div dangerouslySetInnerHTML={{__html: job.how_to_apply}} />
+                    </div>
+                </div>
             </Card.Body>
 
         </Card>
@@ -19,6 +39,9 @@ const styles = {
     cardBody: {
         backgroundColor: 'antiquewhite',
         margin: 10
+    },
+    mainInfo: {
+
     },
     lightText: {
         fontWeight: 'lighter',
