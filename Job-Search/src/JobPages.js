65f29e4a5bb145e9b8aaadd87186b1978fc4524c
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-28 23:22:24 -0400

    complete pagination

--- Job-Search/src/JobPages.js
+++ Job-Search/src/JobPages.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { Pagination } from 'react-bootstrap'
 
 export default function JobPages({page, setPage, hasNextPage}) {
+
+    function adjustPage(change) {
+        setPage(pg => pg + change)
+    }
+
     return (
         <Pagination>
-            { page !== 1 && <Pagination.Prev />}
-            { page !== 1 && <Pagination.Item>{page-1}</Pagination.Item>}
+            { page !== 1 && <Pagination.Prev onClick={()=> adjustPage(-1)} />}
             { page > 2 && <Pagination.Item>1</Pagination.Item>}
             { page > 2 && <Pagination.Ellipsis />}
+            { page !== 1 && <Pagination.Item onClick={()=> setPage(-1)} >{page-1}</Pagination.Item>}
             <Pagination.Item>{page}</Pagination.Item>
-            {hasNextPage && <Pagination.Item>{page+1}</Pagination.Item> }
-            {hasNextPage && <Pagination.Next /> }
+            {hasNextPage && <Pagination.Item onClick={()=> adjustPage(1)} >{page+1}</Pagination.Item> }
+            {hasNextPage && <Pagination.Next onClick={()=> adjustPage(1)} /> }
 
         </Pagination>
     )
