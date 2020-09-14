93ae29448a2443ed8f0171b1cd92fdb739f7cdbe
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 22:19:29 -0400

    text component that can be used to make any text tag with dynamic styling

--- user-system/src/Components/Text.js
+++ user-system/src/Components/Text.js
@@ -2,16 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Text(props) {
+
+  const TagType = props.tag;
+  
+  const defaultStyle = {
+    color: props.dm ? 'white': 'black',
+    fontWeight: 444,
+  }
+
   return (
-    <div>
-      
-    </div>
+    <TagType
+      style = {{...defaultStyle, ...props.style}}
+    >
+      {props.text}
+    </TagType>
   )
 }
 
 Text.propTypes = {
+  tag: PropTypes.string,
+  dm: PropTypes.bool
 
 }
+
 
 export default Text
 
