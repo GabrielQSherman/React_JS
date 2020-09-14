d5db2963ef4541b69997c7ecbb8d2f7d4734b31a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 21:57:22 -0400

    Main App component will have darkmode control which will branch to all children

--- user-system/src/App.js
+++ user-system/src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState} from 'react';
 import {BrowserRouter} from 'react-router-dom'
+
 import AppRouter from './Components/AppRouter'
+import Footer from './Components/Footer';
+import Header from './Components/Header'
+import Button from './Components/Button'
 
 function App() {
+
+  const [dm, toggleDm] = useState(false);
+
+
+  const styles = {
+    mainContain: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: dm ? '#111' : 'white' 
+    }
+  }
+
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <div
+      style={{...styles.mainContain}}
+    >
+      <BrowserRouter>
+        <Header dm={dm}/>
+        <Button
+          onClick={()=> toggleDm(!dm)}
+          text={ dm ? 'Switch To Light Mode' : 'Switch To Dark Mode'  }
+        />
+        <AppRouter dm={dm} />
+        <Footer dm={dm}/>
+      </BrowserRouter>
+    </div>
   );
 }
+
+
 
 export default App;
