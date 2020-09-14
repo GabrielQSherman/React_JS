476472ca200c8bca9348b675d577037af82ed1ce
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 22:18:44 -0400

    basic register-page component, styles according to darkmode

--- user-system/src/Components/Register.js
+++ user-system/src/Components/Register.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Text from './Text'
 
 function Register(props) {
   return (
     <div>
-      
+      <Text 
+        dm={props.dm} 
+        tag='h3'
+        text='Register'
+      >
+      </Text>
     </div>
   )
 }
 
 Register.propTypes = {
-
+  dm: PropTypes.bool
 }
 
 export default Register
