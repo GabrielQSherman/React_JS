c2e8df5006fe5b254ae06fc18072d5abd7e67968
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 22:17:36 -0400

    complete basic structer of page footer

--- user-system/src/Components/Footer.js
+++ user-system/src/Components/Footer.js
@@ -3,13 +3,23 @@ import PropTypes from 'prop-types'
 
 function Footer(props) {
   return (
-    <div>
-      
+    <div
+      style={{
+        position:'absolute',
+        bottom:0,
+        width:'100%',
+        height:'60px',
+        fontWeight: 100,
+        color: props.dm ? 'grey' : 'black',
+      }}
+    >
+      Certainly not a copy of GitHub
     </div>
   )
 }
 
 Footer.propTypes = {
+  dm: PropTypes.bool
 
 }
 
