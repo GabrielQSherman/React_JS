8819d047022ae78b35d5f44ff0d2156fd9521534
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 22:18:08 -0400

    basic home component, styles according to darkmode

--- user-system/src/Components/Home.js
+++ user-system/src/Components/Home.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Text from './Text'
 
 function Home(props) {
   return (
     <div>
-      
+      <Text 
+        dm={props.dm} 
+        tag='h3'
+        text='Home'
+      >
+      </Text> 
     </div>
   )
 }
 
 Home.propTypes = {
-
+  dm: PropTypes.bool
 }
 
 export default Home
