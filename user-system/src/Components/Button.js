0bc7c445e6040d0d44c9bd3f3f1505520b279049
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 22:11:04 -0400

    button component basics complete

--- user-system/src/Components/Button.js
+++ user-system/src/Components/Button.js
@@ -2,14 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Button(props) {
+
+  const defaultStyle = {
+    color: props.dm ? 'white' : 'black',
+    backgroundColor: !props.dm ? 'white' : 'black',
+    padding: 10,
+    borderRadius: 5,
+    margin: 1
+  }
+
   return (
-    <div>
-      
-    </div>
+    <button
+      style={{...defaultStyle, ...props.style}}
+      onClick = {props.onClick}
+    >{props.text}</button>
   )
 }
 
+
 Button.propTypes = {
+  style: PropTypes.object,
+  dm: PropTypes.bool
 
 }
 
