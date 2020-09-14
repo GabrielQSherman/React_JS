1642004d20e26e5accfb2d2bc3504ac651cc687b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 21:58:00 -0400

    add in pages with approuter

--- user-system/src/Components/AppRouter.js
+++ user-system/src/Components/AppRouter.js
@@ -1,15 +1,30 @@
 import React from 'react'
+
+import { Switch, Route } from 'react-router-dom'
+
 import PropTypes from 'prop-types'
+import Home from './Home'
+import Login from './Login'
+import Register from './Register'
 
 function AppRouter(props) {
   return (
-    <div>
-      
-    </div>
+      <Switch>
+        <Route exact path='/'>
+          <Home dm={props.dm}/>
+        </Route>
+        <Route exact path='/login'>
+          <Login dm={props.dm}/>
+        </Route>
+        <Route exact path='/register'>
+          <Register dm={props.dm}/>
+        </Route>
+      </Switch>
   )
 }
 
 AppRouter.propTypes = {
+  dm: PropTypes.bool.isRequired
 
 }
 
