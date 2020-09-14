8508adaf3b91e3c007ddb05f84b4edd3c2eabf3d
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 22:18:34 -0400

    basic login-page component, styles according to darkmode

--- user-system/src/Components/Login.js
+++ user-system/src/Components/Login.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Text from './Text'
 
 function Login(props) {
   return (
     <div>
-      
+      <Text 
+        dm={props.dm} 
+        tag='h3'
+        text='Login'
+      >
+      </Text>
     </div>
   )
 }
 
 Login.propTypes = {
-
+  dm: PropTypes.bool
 }
 
 export default Login
