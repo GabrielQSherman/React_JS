2dfb99e4a0edb1c405a6b71fe740a6f116cd1c7b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-13 22:23:52 -0400

    basic structure for any input element

--- user-system/src/Components/Input.js
+++ user-system/src/Components/Input.js
@@ -2,15 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function Input(props) {
+
+  const defaultStyle = {
+    color: props.dm ? 'white' : 'black',
+    backgroundColor: props.dm ? '#111' : 'white',
+    borderRadius: 5,
+    border: 'none',
+    padding: 10,
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <input
+
+      style={{...defaultStyle, ...props.style}}
+      type={props.type}
+      placeholder={props.ph}
+      onChange={props.onChange}
+    
+    />
   )
 }
 
 Input.propTypes = {
-
+  dm: PropTypes.bool,
+  style: PropTypes.object,
+  type: PropTypes.string,
+  ph: PropTypes.string,
+  onChange: PropTypes.func,
 }
 
 export default Input
