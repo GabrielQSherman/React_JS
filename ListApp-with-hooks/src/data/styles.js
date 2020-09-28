8106e9977fbf3c3b7bb18de9c2ac7471427e20c5
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-27 23:56:30 -0400

    update form style

--- ListApp-with-hooks/src/data/styles.js
+++ ListApp-with-hooks/src/data/styles.js
@@ -1,4 +1,4 @@
 module.exports =  {
   mainAppDiv: {marginTop: 100},
-  defaultForm: {display: 'flex', flexDirection: 'column', textAlign: 'center', margin: 50, padding: 50}
+  defaultForm: {display: 'flex', flexDirection: 'column', textAlign: 'center', margin: 'auto', padding: 50, maxWidth: 400}
 }
