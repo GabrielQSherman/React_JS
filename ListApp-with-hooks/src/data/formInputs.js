28125a68449126abaa9894d30ddb613441f729fc
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-03 18:25:16 -0400

    add inputs to make a new form

--- ListApp-with-hooks/src/data/formInputs.js
+++ ListApp-with-hooks/src/data/formInputs.js
@@ -2,6 +2,10 @@ module.exports = {
   cryptoFormInputs: [
     {name: 'name', ph: 'Enter The Crypto\'s name' },
     {name: 'ticker', ph: 'Enter The Crypto\'s ticker code' }
+  ],
+  newListInputs: [
+    { name: 'listName', ph: 'Enter Your New List\'s Name' },
+    { name: 'dataName', ph: 'Enter What The Name Of Your Data is'}
   ]
 }
  
