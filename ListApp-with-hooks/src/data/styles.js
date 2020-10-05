73bced43d10ee85e418d19f42058c0dad2122ef0
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-04 20:22:09 -0400

    update style for form so placeholders can fully be seen

--- ListApp-with-hooks/src/data/styles.js
+++ ListApp-with-hooks/src/data/styles.js
@@ -1,5 +1,5 @@
 module.exports =  {
   mainAppDiv: {marginTop: 100},
-  defaultForm: {display: 'flex', flexDirection: 'column', textAlign: 'center', margin: 'auto', padding: 50, maxWidth: 400},
+  defaultForm: {display: 'flex', flexDirection: 'column', textAlign: 'center', margin: 'auto', padding: 50, minWidth: 400},
   defaultInput: {margin: 10, padding: 12, border: 'none', borderRadius: 7, textAlign: 'center'}
 }
