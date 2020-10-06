8348c7dc63cadd9499fc9ae240ac41789bb31eaa
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-06 15:37:46 -0400

    allow the hotel data alg to work for rooms up to 99 and change page styling to a dark theme

--- react-hotel/src/utils/createHotelData.js
+++ react-hotel/src/utils/createHotelData.js
@@ -1,10 +1,3 @@
-/*
-This function will return an 2D array of the hotel room data that will be stored in state. 
-It takes the params of how many floors and how many rooms per floor
-The formula to calculate how much a room cost is as follows
-( $50 x floorNum ) + ( $10 x roomNum(ones place) - $10) + fixedDeposit($45)
-
-*/
 
 const calcHotelData = (floors,rooms) => { //floors: how many floors in the hotel, rooms: how many rooms per floor in the hotel
   const 
@@ -21,7 +14,7 @@ const calcHotelData = (floors,rooms) => { //floors: how many floors in the hotel
       room = j+1,
       floor = i+1, 
       roomData = {
-        room: `${(i+1)*10}${j+1}`,
+        room: `${(floor)}${room < 10 ? '0': '' }${room}`,
         renter: null,
         price: (floorCost*floor) + (roomCost*room) - roomCost + deposit,
       }
@@ -32,7 +25,4 @@ const calcHotelData = (floors,rooms) => { //floors: how many floors in the hotel
   }
   return data
 }
-
-console.log(calcHotelData(4,4));
-
 module.exports = calcHotelData;
