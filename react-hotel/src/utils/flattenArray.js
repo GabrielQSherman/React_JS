7de998279092b157b2e486c5557df5f9d6307980
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-06 19:58:21 -0400

    create style object for inline styling and simple utility function to flatten multi-dimensional  arrays

---
+++ react-hotel/src/utils/flattenArray.js
@@ -0,0 +1,7 @@
+module.exports = (arr) => {
+
+  const newArr = [...arr];
+
+  return newArr.flat(Infinity)
+
+}
---
+++ react-hotel/src/utils/styles.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roomContain: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    textAlign: 'center',
+    justifyContent: 'center'
+  },
+  roomDiv: {
+    margin: 50,
+    padding: 20,
+    backgroundColor: '#222',
+    borderRadius: 14
+  }
+}
