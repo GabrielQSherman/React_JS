ba3fbbaa0431a3c035d2132b54ac12ec362905da
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-06 22:25:30 -0400

    update styles and implement unique id into rooms when data is first initalized

--- react-hotel/src/utils/createHotelData.js
+++ react-hotel/src/utils/createHotelData.js
@@ -1,3 +1,4 @@
+ const {v1 : uuid} = require( 'uuid' )
 
 const calcHotelData = (floors,rooms) => { //floors: how many floors in the hotel, rooms: how many rooms per floor in the hotel
   const 
@@ -11,9 +12,11 @@ const calcHotelData = (floors,rooms) => { //floors: how many floors in the hotel
     data.push([])
     for (let j = 0; j < rooms; j++) {
       const
+      id = uuid(),
       room = j+1,
       floor = i+1, 
       roomData = {
+        id: id,
         room: `${(floor)}${room < 10 ? '0': '' }${room}`,
         renter: null,
         price: (floorCost*floor) + (roomCost*room) - roomCost + deposit,
--- react-hotel/src/utils/styles.js
+++ react-hotel/src/utils/styles.js
@@ -1,4 +1,7 @@
 module.exports = {
+  mainDiv: {
+    marginTop: 100,
+  },
   roomContain: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -10,5 +13,14 @@ module.exports = {
     padding: 20,
     backgroundColor: '#222',
     borderRadius: 14
+  },
+  button: {
+    borderRadius: 20,
+    padding: 15,
+    cursor: 'pointer',
+    transition: 'all ease-in-out .5s',
+    border: 'none',
+    color: 'white',
+    backgroundColor: '#333',
   }
 }
